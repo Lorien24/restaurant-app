4e6bcc72bf7913585faed7dce5cc9f7762ea27ff
Lorien24 <tokillaggnore@gmail.com>
2022-04-03 14:30:34 +0300

    restaurant app

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const [toggleMenu, setToggleMenu] = useState(false);
   return(
   <nav className='app__navbar'>
     <div className='app__navbar-logo'>
-      <img src={images.lorien} alt="app logo" />
+      <img src={images.gericht} alt="app logo" />
     </div>
     <ul className='app__navbar-links'>
       <li className='p__opensans'>
